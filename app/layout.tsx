85680c89a39be23e04f4bb90f25d6b8a476b53b1
Dekatron322 <muritalaibrahim097@gmail.com>
2023-08-21 00:10:33 +0100

    meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,43 @@
-import { Footer, Founder, Investors, Navbar } from '@/components'
-import './globals.css'
-import type { Metadata } from 'next'
-
+import { Footer, Founder, Investors, Navbar } from "@/components";
+import "./globals.css";
+import type { Metadata } from "next";
+import Head from "next/head";
 
 export const metadata: Metadata = {
-  title: 'TheSurge.tech - Igniting Innovation in Tech Startups',
-  description: 'TheSurge.tech is a pioneering force dedicated solely to empowering tech startups across Africa. Our mission is to revolutionize the tech startup ecosystem, providing comprehensive support and resources to fuel innovation, accelerate growth, and drive unprecedented success.',
-}
+  title: "TheSurge.tech - Igniting Innovation in Tech Startups",
+  description:
+    "TheSurge.tech is a pioneering force dedicated solely to empowering tech startups across Africa. Our mission is to revolutionize the tech startup ecosystem, providing comprehensive support and resources to fuel innovation, accelerate growth, and drive unprecedented success.",
+  keywords:
+    "Startups, Funding, Angel, Investment, Investor, Capital, Seed Funding, African, ",
+  twitter: {
+    card: "summary_large_image",
+    title: "TheSurge",
+    images:
+      "https://github.com/GreenInkLimited/PurplePagesWeb/blob/main/src/assets/homepage.png?raw=true",
+    description:
+      "TheSurge.tech is a pioneering force dedicated solely to empowering tech startups across Africa. Our mission is to revolutionize the tech startup ecosystem, providing comprehensive support and resources to fuel innovation, accelerate growth, and drive unprecedented success.",
+  },
+  openGraph: {
+    title: "TheSurge",
+    images:
+      "https://github.com/GreenInkLimited/PurplePagesWeb/blob/main/src/assets/homepage.png?raw=true",
+    description:
+      "TheSurge.tech is a pioneering force dedicated solely to empowering tech startups across Africa. Our mission is to revolutionize the tech startup ecosystem, providing comprehensive support and resources to fuel innovation, accelerate growth, and drive unprecedented success.",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      
       <body className="relative">
-      <Navbar />
+        <Navbar />
         {children}
-      <Footer />
+        <Footer />
       </body>
-      
     </html>
-  )
+  );
 }
